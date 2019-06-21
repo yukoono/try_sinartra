37f14c6ca60e0025263a99d3b3933e6a153b8a8a
yukoono <yuko.ono@classi.jp>
2019-06-21 16:08:10 +0900

    try_sinatra

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ require "sinatra"
 require "sinatra/reloader"
 require "time"
 
+def character?(input)
+  if !input.nil?
+    !input.delete("\n").delete("\r").empty?
+  else
+    false
+  end
+end
+
 get "/" do
   directry_name = ARGV[0] || Dir.pwd + "/memo"
   file_names = Dir.entries(directry_name)
@@ -12,14 +20,11 @@ get "/" do
   @memo = {}
   file_names.each do |file_name|
     file_path = "memo/" + file_name
-    File.open(file_path, "r") do |f|
-      while 1 do
-        memo_line = f.gets
-        unless memo_line.nil?
-          unless memo_line.delete("\n").delete("\r").empty?
-            @memo[file_path] = memo_line
-            break
-          end
+    File.open(file_path, "r") do |file|
+      file.each_line do |line|
+        if character?(line)
+          @memo[file_path] = line
+          break
         end
       end
     end
@@ -36,13 +41,10 @@ get "/addition/alert" do
   erb :addition
 end
 
-get "/detail/*" do |file_name|
-  @file_name = file_name
-  @text_lines = []
-  File.open(file_name, "r") do |f|
-    f.each_line do |line|
-      @text_lines << line.gsub(/\n/, "<br/>")
-    end
+get "/detail/*" do |file_path|
+  @file_path = file_path
+  File.open(file_path, "r") do |file|
+    @text = file.read.gsub(/\n/, "<br/>")
   end
   erb :detail
 end
@@ -52,15 +54,17 @@ get "/mv_addition" do
 end
 
 post "/add_text" do
-  unless params[:text].delete("\n").delete("\r").empty?
+  input = params[:text]
+  if character?(input)
     writeable = 0
     while 1 do
       time = Time.now
-      File.open("memo/text#{time}.txt", "a") do |f|
-        if f.flock(File::LOCK_EX | File::LOCK_NB)
-          f.puts params[:text]
+      file_path = "memo/text#{time}.txt"
+      File.open(file_path, "a") do |file|
+        if file.flock(File::LOCK_EX | File::LOCK_NB)
+          file.puts input
           writeable = 1
-          f.flock(File::LOCK_UN)
+          file.flock(File::LOCK_UN)
         end
       end
       if writeable == 1
@@ -73,42 +77,38 @@ post "/add_text" do
   end
 end
 
-get "/change/alert/*" do |file_name|
-  @file_name = file_name
-  @text_lines = []
-  File.open(file_name, "r") do |f|
-    f.each_line do |line|
-      @text_lines << line.gsub(/\n/, "<br/>")
-    end
+get "/change/alert/*" do |file_path|
+  @file_path = file_path
+  File.open(file_path, "r") do |file|
+    @text = file.read.gsub(/\n/, "<br/>")
   end
   @alert = "このメモを消去します。よろしいですか？"
   erb :change_alert
 end
 
-get "/change/*" do |file_name|
-  @file_name = file_name
+get "/change/*" do |file_path|
+  @file_path = file_path
   @text_lines = []
-  File.open(file_name, "r") do |f|
-    f.each_line do |line|
-      @text_lines << line
-    end
+  File.open(file_path, 'r') do |f|
+    @text = f.read.gsub(/\n/, "&#13;")
   end
   erb :change
 end
 
-patch "/changed/*" do |file_name|
-  unless params[:text].delete("\n").delete("\r").empty?
-    File.open(file_name, "w") do |f|
-      f.write params[:text]
+patch "/changed/*" do |file_path|
+  input = params[:text]
+  if character?(input)
+    File.open(file_path, "w") do |file|
+      file.write input
     end
     redirect "/"
   else
-    alert_address = "/change/alert/" + file_name.gsub(/ /, "%20")
-    redirect alert_address
+    alert_uri = "/change/alert/" + file_path.gsub(/ /, "%20")
+    redirect alert_uri
   end
 end
 
-delete "/delete/*" do |file_name|
-  FileUtils.rm(file_name)
+delete "/delete/*" do |file_path|
+  FileUtils.rm(file_path)
   redirect "/"
 end
